7ec4aa570ab447e1e83ff9d055de2990d3d27210
BblChara <a1eksey.bru@yandex.ru>
2022-02-26 00:45:47 +0300

    append nul and negative occasion

--- 8.cpp
+++ 8.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
  long long div1(long long dividend, long long divisor) {
     long long result = 1;
-    long long denominator = divisor;
+    int k = 1;
+
+    if (divisor < 0 && dividend > 0) { divisor *= -1; k = -1; }
+    else if (divisor > 0 && dividend < 0) { dividend *= -1; k = -1;}
+    else if (divisor < 0 && dividend < 0) { dividend *= -1; divisor *= -1; }
+    if ((divisor > dividend) | (divisor ==0) | (dividend ==0)) return 0;
+
     //  Побитовым сдвигом увеличиваем denominator до тех пор, пока оно не станет больше делимого
+    long long denominator = divisor;
     while (dividend > denominator) {
         denominator <<= 1;
         result <<= 1;
@@ -16,7 +23,7 @@ using namespace std;
         result -= 1;
     }
 
-    return result;
+    return k*result;
 }
 
  int main() {
